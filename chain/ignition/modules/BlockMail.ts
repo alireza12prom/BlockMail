43e8431fe7597c9f9d99372da43b9f26a209b096
Alireza Rezapour <hamtapay.rezapour@gmail.com>
2026-01-31 12:22:23 +0330

    🗑️ Remove chain and desktop project files, including configuration, contracts, and dependencies.

--- chain/ignition/modules/BlockMail.ts
+++
@@ -1,6 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("BlockMailModule", (m) => {
-  const mail = m.contract("BlockMail");
-  return { mail };
-});
